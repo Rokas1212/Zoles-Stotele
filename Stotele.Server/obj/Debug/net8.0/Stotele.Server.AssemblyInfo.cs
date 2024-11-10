53556d38d4e7e1bbcf921169240a0373ec49a0d2
Gvidas Valionis <131956175+Laivelis123@users.noreply.github.com>
2024-11-10 12:51:05 +0200

    Reikia dar prideti mesgtamos kategorijos pridejima, nezinau is kur tai padaryti tai gal vakare

--- Stotele.Server/obj/Debug/net8.0/Stotele.Server.AssemblyInfo.cs
+++ Stotele.Server/obj/Debug/net8.0/Stotele.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stotele.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ead95de491bc1623e6c1790e87de8de3b97a74d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b97abff64f7a975c0c9b6ddb541d337a1f472151")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stotele.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stotele.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- stotele.client/src/pages/Rekomendacijos/BlokuotosRekomendacijos.tsx
+++ stotele.client/src/pages/Rekomendacijos/BlokuotosRekomendacijos.tsx
@@ -1,7 +1,19 @@
+import React from "react";
+
 const BlokuotosRekomendacijos = () => {
+  const products = ["Žoliapjovė", "Laistymo žarna", "Trąšų maišas", "Sodo kastuvas"];
+
   return (
     <div>
-      <h1>Blokuotos rekomendacijos ?????</h1>
+      <h1>Blokuotos rekomendacijos</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            {product}
+            <button style={{ marginLeft: "10px" }}>Pašalinti</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
