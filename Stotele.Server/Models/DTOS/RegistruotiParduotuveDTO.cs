e0b8a35f939e108355e8506363b3cc02a4f837fb
Gvidas Valionis <131956175+Laivelis123@users.noreply.github.com>
2024-12-14 19:01:54 +0200

    Add shop/manager creation

---
+++ Stotele.Server/Models/DTOS/RegistruotiParduotuveDTO.cs
@@ -0,0 +1,14 @@
+namespace Stotele.Server.Models
+{
+    public class RegistruotiParduotuveDTO
+    {
+        public string DarboLaikoPradzia { get; set; }
+        public string DarboLaikoPabaiga { get; set; }
+        public double Kvadratura { get; set; }
+        public int DarbuotojuKiekis { get; set; }
+        public string TelNumeris { get; set; }
+        public string ElPastas { get; set; }
+        public string Faksas { get; set; }
+        public string Adresas { get; set; }
+    }
+}
---
+++ Stotele.Server/Models/DTOS/RegistruotiVadybininkaDTO.cs
@@ -0,0 +1,18 @@
+namespace Stotele.Server.Models
+{
+    public class RegistruotiVadybininkaDTO
+    {
+        public string Lytis { get; set; }
+        public string ElektroninisPastas { get; set; }
+        public string Slaptazodis { get; set; }
+        public string Vardas { get; set; }
+        public string Slapyvardis { get; set; }
+        public string Pavarde { get; set; }
+        public string Miestas { get; set; }
+        public string Adresas { get; set; }
+        public int PastoKodas { get; set; }
+        public DateTime GimimoData { get; set; }
+        public int ParduotuveId { get; set; }
+        public string Skyrius { get; set; }
+    }
+}
